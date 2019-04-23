2959a1ede16fb8149da9c7417ba538e8a450c271
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-04-22 17:17:37 -0700

    use block-ark to test scala export

--- test/basic.js
+++ test/basic.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const chai = require('chai');
+const jsonRefs = require('json-refs');
+const duhCore = require('duh-core');
+
 const lib = require('../lib/index.js');
+
+require('json5/lib/register');
+const ark = require('block-ark');
 
 const expect = chai.expect;
 
@@ -14,7 +20,7 @@ const comp = {
   }
 };
 
-describe('basic', () => {
+describe('empty', () => {
   it('empty-base', done => {
     expect(lib.exportScalaBase(comp)).to.be.a('string');
     done();
@@ -22,6 +28,22 @@ describe('basic', () => {
   it('empty-user', done => {
     expect(lib.exportScalaUser(comp)).to.be.a('string');
     done();
+  });
+});
+
+describe('ark', () => {
+  jsonRefs.resolveRefs(ark).then(res => {
+    const duh1 = res.resolved;
+    duhCore.expandAll(duh1).then(duh2 => {
+      it('ark-base', done => {
+        expect(lib.exportScalaBase(duh2)).to.be.a('string');
+        done();
+      });
+      it('ark-user', done => {
+        expect(lib.exportScalaUser(duh2)).to.be.a('string');
+        done();
+      });
+    });
   });
 });
 
